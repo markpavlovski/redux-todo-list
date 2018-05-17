c8a13525f96869394217d9c302ad540476997d3f
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-17 10:12:00 -0700

    added a reducer function

--- src/reducers.js
+++ src/reducers.js
@@ -1,2 +1,13 @@
 import { combineReducers } from 'redux'
-export default combineReducers({})
+
+const INITIAL_VALUE = [
+  {id: 'HkrtzegRz', task: 'teach react', checked: false},
+  {id: 'Hyu3GlxAG', task: 'learn react', checked: true},
+  {id: 'B19TGge0z', task: 'develop with react', checked: false}
+]
+
+function toDoList(data = INITIAL_VALUE) {
+  return data
+}
+
+export default combineReducers({toDoList})
